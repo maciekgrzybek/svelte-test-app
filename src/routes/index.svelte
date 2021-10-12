6e1149c3d98f07f050b855966f7897618debd657
Maciej Grzybek <maciej.grzybek@karnovgroup.com>
2021-10-12 15:19:14 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,42 @@
+<script>
+	import { inview } from 'svelte-inview';
+
+	let isInView;
+	const options = { unobserveOnEnter: false };
+</script>
+
+<main>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
+		apps.
+	</p>
+</main>
+
+<div use:inview={options} on:inviewChange={({ detail: { inView } }) => (isInView = inView)} on>
+	{#if isInView}
+		Stuff here...
+	{/if}
+</div>
+
+<style>
+	main {
+		height: 1200px;
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
